8de775e591b276b148063822acc52a4f6ad93155
Isaac H <kijijitoys@gmail.com>
2024-08-17 11:10:24 -0600

    Add files via upload

---
+++ ECE 252/lab1/catpng.c
@@ -0,0 +1,217 @@
+/********************************************************************
+ * @file: catpng.c
+ * @brief: Concatenate pngs
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "./starter/png_util/lab_png.h"
+#include "./starter/png_util/zutil.c"
+#include "./starter/png_util/crc.c"
+
+#define PNG_SIGNATURE_SIZE 8
+#define CHUNK_TYPE_SIZE 4
+#define CHUNK_CRC_SIZE 4
+#define CHUNK_LENGTH_SIZE 4
+
+unsigned int hexToDec(unsigned char hex[], int length) {
+    int result = 0;
+    for (int i = 0; i < length; i++) {
+        result |= hex[i] << (8 * (length - 1 - i));
+    }
+    return result;
+}
+
+int getWidth(FILE* fp) {
+    unsigned char width[4];
+    fseek(fp, 16, SEEK_SET);
+    fread(width, 4, 1, fp);
+    int result = hexToDec(width, 4);
+    return result;
+}
+
+int getHeight(FILE* fp) {
+    unsigned char height[4];
+    fseek(fp,20, SEEK_SET);
+    fread(height, 4, 1, fp);
+    int result = hexToDec(height, 4);
+    return result;
+}
+
+int is_png(U8 *buf, size_t n)
+{
+    if (n < 8) {
+        return 0; // Not enough data to verify PNG signature
+    }
+
+    // PNG file signature
+    U8 png_signature[8] = {0x89, 'P', 'N', 'G', 0x0D, 0x0A, 0x1A, 0x0A};
+    
+    // Compare the file signature with the PNG signature
+    if (memcmp(buf, png_signature, 8) == 0) {
+        return 1; // It's a PNG file
+    } else {
+        return 0; // Not a PNG file
+    }
+}
+
+int main(int argc, char *argv[]) 
+{
+    if(argc < 2) {
+        printf("not enough arguments");
+        return 1;
+    }
+
+    int width = 0;
+    int catHeight = 0;
+    unsigned char *IDAT_data = (unsigned char *)malloc(0 * sizeof(unsigned char));
+    unsigned long IDAT_data_array_length = 0;
+    U64 inflate = 0;
+    U64 deflate = 0;
+    
+    for (int i = 1; i < argc; i++) {
+        //can't count arg 0
+        
+        FILE *fp;
+        fp = fopen(argv[i], "rb");
+        
+        U8 isPNG_signature[8];
+        if (fread(isPNG_signature, 1, 8, fp) != 8) {
+            perror("fread signature");
+            fclose(fp);
+            continue;
+        }
+        if (!is_png(isPNG_signature, 8)) {
+            printf("%s: Not a PNG file\n", argv[i]);
+            fclose(fp);
+            continue;
+        }
+
+        width = getWidth(fp);
+        unsigned char IDAT_size[4];
+
+        fseek(fp, 33, SEEK_SET);
+        fread(IDAT_size, 4, 1, fp);
+
+        U64 IDAT_length = hexToDec(IDAT_size, 4);
+        U8 IDAT_data_length[IDAT_length]; //length of IDAT data
+
+        fseek(fp, 41, SEEK_SET);
+        fread(IDAT_data_length, IDAT_length, 1, fp);
+
+        deflate += IDAT_length;
+        //we can assume all png widths are the same, no need to check different widths
+        int currentWidth = getWidth(fp);
+        int currentHeight = getHeight(fp);
+
+        catHeight += currentHeight;
+        width = currentWidth; //doesn't really do anything but allows to use width at the very end for writing output
+
+        U64 size = currentHeight * (currentWidth * 4 + 1);
+        U8 dest[size];
+
+        int inf = mem_inf(dest, &size, IDAT_data_length, IDAT_length);
+        IDAT_data = (unsigned char *) realloc(IDAT_data, (catHeight * (currentWidth * 4 + 1)) * sizeof(unsigned char));
+        
+        for (int i = 0; i < size; i++) {
+            IDAT_data[inflate + i]= dest[i];
+        }
+        inflate += size; 
+
+        fclose(fp);
+    }
+    unsigned char IDAT_data_output[deflate];
+    int def = mem_def(IDAT_data_output, &deflate, IDAT_data, inflate, Z_DEFAULT_COMPRESSION);
+
+    //every write needs to go through htonl()
+
+    FILE *file; //used to get the header and footer data again
+    for (int i = 1; i < argc; i++) {
+        file = fopen(argv[i], "rb");
+        U8 isPNG_signature[8];
+        if (fread(isPNG_signature, 1, 8, file) != 8) {
+            perror("fread signature");
+            continue;
+        }
+        if (!is_png(isPNG_signature, 8)) {
+            continue;
+        }
+        break;
+    }
+
+    FILE *output = fopen("all.png", "w+");
+    unsigned char temp[8] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+    fwrite(temp, 8, 1, output); //write "png" header
+
+    unsigned char IHDR_type[8];
+    fseek(file, 8, SEEK_SET);
+    fread(IHDR_type, 8, 1, file);
+    fwrite(IHDR_type, 8, 1, output); //copy length chunk
+
+    //write height and width
+    U32 IHDR_width = (U32) htonl(width);
+    fwrite(&IHDR_width, 4, 1, output);
+
+    U32 IHDR_height = (U32) htonl(catHeight);
+    fwrite(&IHDR_height, 4, 1, output);
+
+    //write bit depth, etc.
+    unsigned char IHDR_misc[5];
+    fseek(file, 24, SEEK_SET);
+    fread(IHDR_misc, 5, 1, file);
+    fwrite(IHDR_misc, 5, 1, output);
+
+    //calculate and write IHDR CRC
+    unsigned char temp55[17];
+    fseek(output, 12, SEEK_SET); //skip png header and length chunk
+    fread(temp55, 17, 1, output);
+    unsigned long IHDR_CRC = crc(temp55, 17);
+    U32 IHDR_CRC32 = (U32) htonl(IHDR_CRC);
+    fwrite(&IHDR_CRC32, 4, 1, output);
+
+    //write IDAT length
+    U32 deflate32 = (U32) htonl(deflate);
+    fwrite(&deflate32, 4, 1, output);
+
+    //write IDAT type
+    unsigned char IDAT_type[4];
+    fseek(file, 37, SEEK_SET);
+    fread(IDAT_type, 4, 1, file);
+    fwrite(IDAT_type, 4, 1, output);
+
+    //write IDAT data may need break point before this to see if bytes before are correct
+    //https://hexed.it/
+    fwrite(&IDAT_data_output, 1, deflate, output);
+
+    //write IDAT CRC
+    unsigned char temp2[deflate+4];
+    fseek(output, 37, SEEK_SET); //skip header & IHDR
+    fread(temp2, 1, deflate + 4, output); //read type and data
+    unsigned long IDAT_CRC = crc(temp2, deflate + 4);
+    U32 IDAT_CRC32 = (U32) htonl(IDAT_CRC);
+    fwrite(&IDAT_CRC32, 4, 1, output);
+
+    //write IEND length and type
+    U32 len = 0;
+    unsigned char chunkType[4] = {'I','E','N','D'};
+    fwrite(&len, sizeof(len), 1, output); //0 length
+    for(int i = 0; i < 4; i++) {
+        fputc(chunkType[i], output); //put IEND in type
+    }
+
+    //write IEND CRC
+    unsigned char temp3[4];
+    fseek(output, 49 + deflate, SEEK_SET);
+    fread(temp3, 1, 4, output);
+    unsigned long IEND_CRC = crc(temp3, 4);
+    U32 IEND_CRC32 = (U32) htonl(IEND_CRC);
+    fwrite(&IEND_CRC32, 4, 1, output);
+
+    //clean up
+    fclose(file);
+    fclose(output);
+    free(IDAT_data);
+
+    return 0;
+}
---
+++ ECE 252/lab1/findpng-fix2.c
@@ -0,0 +1,310 @@
+/********************************************************************
+ * @file: findpng.c
+ * @brief: List all pngs within a given directory
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <stdbool.h>
+
+
+#define PNG_SIG_SIZE    8 /* number of bytes of png image signature data */
+#define CHUNK_LEN_SIZE  4 /* chunk length field size in bytes */          
+#define CHUNK_TYPE_SIZE 4 /* chunk type field size in bytes */
+#define CHUNK_CRC_SIZE  4 /* chunk CRC field size in bytes */
+#define DATA_IHDR_SIZE 13 /* IHDR chunk data field size */
+
+/******************************************************************************
+ * STRUCTURES and TYPEDEFS 
+ *****************************************************************************/
+typedef unsigned char U8;
+typedef unsigned int  U32;
+
+typedef struct chunk {
+    U32 length;  /* length of data in the chunk, host byte order */
+    U8  type[4]; /* chunk type */
+    U8  *p_data; /* pointer to location where the actual data are */
+    U32 crc;     /* CRC field  */
+} *chunk_p;
+
+/* note that there are 13 Bytes valid data, compiler will padd 3 bytes to make
+   the structure 16 Bytes due to alignment. So do not use the size of this
+   structure as the actual data size, use 13 Bytes (i.e DATA_IHDR_SIZE macro).
+ */
+//#pragma pack(push, 1)
+typedef struct data_IHDR {// IHDR chunk data 
+    U32 width;        /* width in pixels, big endian   */
+    U32 height;       /* height in pixels, big endian  */
+    U8  bit_depth;    /* num of bits per sample or per palette index.
+                         valid values are: 1, 2, 4, 8, 16 */
+    U8  color_type;   /* =0: Grayscale; =2: Truecolor; =3 Indexed-color
+                         =4: Greyscale with alpha; =6: Truecolor with alpha */
+    U8  compression;  /* only method 0 is defined for now */
+    U8  filter;       /* only method 0 is defined for now */
+    U8  interlace;    /* =0: no interlace; =1: Adam7 interlace */
+} *data_IHDR_p;
+//#pragma pack(pop)
+
+/* A simple PNG file format, three chunks only*/
+typedef struct simple_PNG {
+    struct chunk *p_IHDR;
+    struct chunk *p_IDAT;  /* only handles one IDAT chunk */  
+    struct chunk *p_IEND;
+} *simple_PNG_p;
+
+
+/******************************************************************************
+ * Functions 
+ *****************************************************************************/
+
+/* Table of CRCs of all 8-bit messages. */
+unsigned long crc_table[256];
+
+/* Flag: has the table been computed? Initially false. */
+int crc_table_computed = 0;
+
+/* Make the table for a fast CRC. */
+void make_crc_table(void)
+{
+    unsigned long c;
+    int n, k;
+
+    for (n = 0; n < 256; n++) {
+        c = (unsigned long) n;
+        for (k = 0; k < 8; k++) {
+            if (c & 1)
+                c = 0xedb88320L ^ (c >> 1);
+            else
+                c = c >> 1;
+        }
+        crc_table[n] = c;
+    }
+    crc_table_computed = 1;
+}
+
+/* Update a running CRC with the bytes buf[0..len-1]--the CRC
+   should be initialized to all 1's, and the transmitted value
+   is the 1's complement of the final running CRC (see the
+   crc() routine below)). */
+
+unsigned long update_crc(unsigned long crc, unsigned char *buf, int len)
+{
+    unsigned long c = crc;
+    int n;
+
+    if (!crc_table_computed)
+        make_crc_table();
+    for (n = 0; n < len; n++) {
+        c = crc_table[(c ^ buf[n]) & 0xff] ^ (c >> 8);
+    }
+    return c;
+}
+
+/* Return the CRC of the bytes buf[0..len-1]. */
+unsigned long crc(unsigned char *buf, int len)
+{
+    return update_crc(0xffffffffL, buf, len) ^ 0xffffffffL;
+}
+
+
+int get_png_data_IHDR(struct data_IHDR *out, FILE *fp, long offset, int whence) {
+    // Seek to the offset where IHDR chunk is expected
+    if (fseek(fp, offset, whence) != 0) {
+        perror("fseek");
+        return -1;
+    }
+
+    // Read the length of the IHDR chunk (should be 13 for IHDR)
+    U32 length;
+    if (fread(&length, sizeof(length), 1, fp) != 1) {
+        perror("fread length");
+        return -1;
+    }
+
+    length = ntohl(length);
+
+    if (length != 13) {
+        fprintf(stderr, "Invalid IHDR chunk length: %u\n", length);
+        return -1;
+    }
+
+    // Read the IHDR chunk type (should be 'IHDR')
+    U8 chunk_type[4];
+    if (fread(chunk_type, sizeof(chunk_type), 1, fp) != 1) {
+        perror("fread chunk type");
+        return -1;
+    }
+
+    if (memcmp(chunk_type, "IHDR", 4) != 0) {
+        fprintf(stderr, "Expected IHDR chunk, but found: %c%c%c%c\n", chunk_type[0], chunk_type[1], chunk_type[2], chunk_type[3]);
+        return -1;
+    }
+
+    if (fread(out, DATA_IHDR_SIZE, 1, fp) != 1) {
+        perror("fread IHDR data");
+        return -1;
+    }
+
+    U32 actual_crc;
+    if (fread(&actual_crc, sizeof(actual_crc), 1, fp) != 1) {
+        perror("fread CRC");
+        return -1;
+    }
+    actual_crc = ntohl(actual_crc);
+
+    unsigned char crc_buffer[4 + DATA_IHDR_SIZE];
+    memcpy(crc_buffer, chunk_type, 4);
+    memcpy(crc_buffer + 4, out, DATA_IHDR_SIZE);
+
+    unsigned long expected_crc = crc(crc_buffer, 4 + DATA_IHDR_SIZE);
+
+    out->width = ntohl(out->width);
+    out->height = ntohl(out->height);
+
+    if (actual_crc != expected_crc) {
+        fprintf(stderr, "IHDR chunk CRC error: computed %08lx, expected %08x\n", expected_crc, actual_crc);
+        return -1;
+    }
+
+    return 0;
+}
+
+
+int is_png(char*possible_png_file)
+{
+    FILE *fp = fopen(possible_png_file, "rb");
+    if (!fp) {
+        return 0;
+    }
+    
+    U8 signature[8];
+    if (fread(signature, 1, 8, fp) != 8) {
+        fclose(fp);
+    }
+
+    /*/ PNG file signature /*/
+    U8 png_signature[8] = {0x89, 'P', 'N', 'G', 0x0D, 0x0A, 0x1A, 0x0A};
+    
+    /*/ Compare the file signature with the PNG signature /*/
+    if (memcmp(signature, png_signature, 8) == 0) 
+    {
+        /*/ CHECK CRC /*/
+        struct data_IHDR ihdr;
+        if (get_png_data_IHDR(&ihdr, fp, 8, SEEK_SET) != 0) 
+        {
+            fclose(fp);
+            return 1;
+        }
+
+        fclose(fp);
+        return 0; /*It is a PNG*/
+    }
+    else 
+    {
+        fclose(fp);
+        return 1; /*Not a PNG*/
+    }
+}
+
+
+/*/The scavenge function "scavenges" through the given directory printing out all paths to a valid png/*/
+int scavenge(char*search_directory, char*home)
+{
+    /*/////////////// lists file names in directory ///////////////*/
+    DIR *p_dir;
+    struct dirent *p_dirent;
+    char str[64];
+    int empty_directory = 1;
+
+    /* maybe move to main */
+    if ((p_dir = opendir(search_directory)) == NULL) 
+    {
+        sprintf(str, "opendir(%s)", search_directory);
+        perror(str);
+        exit(2);
+    }
+
+    while ((p_dirent = readdir(p_dir)) != NULL) 
+    {
+        if (strcmp(p_dirent->d_name, ".") == 0) 
+        {
+            continue;
+        }
+        if (strcmp(p_dirent->d_name, "..") == 0) 
+        {
+            continue;
+        }
+        
+        char Path[4096]; /*/ Overall path that has been taken /*/
+        snprintf(Path, sizeof(Path), "%s/%s", search_directory, p_dirent->d_name);
+        
+        struct stat buf;
+        stat(Path, &buf);
+        
+        if (S_ISREG(buf.st_mode)) /* Is Regular File */
+        {
+            if (is_png(Path) == 0) 
+            {
+                empty_directory = 0;
+                char pngPath[4096];
+                snprintf(pngPath, sizeof(pngPath), "%s/%s", home, p_dirent->d_name);
+                printf("%s\n", pngPath);
+            }
+        } 
+        else if (S_ISDIR(buf.st_mode)) /* Is Directory */
+        {
+            char newPath[4069];
+            snprintf(newPath, sizeof(newPath), "%s/%s", home, p_dirent->d_name);
+            int hold = scavenge(Path, newPath);
+            empty_directory = hold && empty_directory;
+        }
+        else if (S_ISLNK(buf.st_mode))
+        {
+            continue;
+        }
+    }
+
+    if ( closedir(p_dir) != 0 ) {
+        perror("closedir");
+        exit(3);
+    }
+
+    if (empty_directory == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+    /*//////////////////////////////////////////////////////////////*/
+
+}
+
+
+/****************************************************************************** 
+ *****************************************************************************/
+
+
+int main(int argc, char *argv[]) 
+{
+    if (argc == 1) {
+        fprintf(stderr, "Usage: %s <directory name>\n", argv[0]);
+        exit(1);
+    }
+
+    char*search_directory = argv[1];
+    
+    if (scavenge(search_directory, ".") == 1)
+    {
+        printf("findpng: No PNG file found\n");
+    }
+    
+    return 0;
+
+}
